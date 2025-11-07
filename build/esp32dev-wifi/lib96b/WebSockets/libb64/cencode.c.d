b21fb993f14dd30593deb562bf9a8b4d47c89ad2
haryasanta <haryasanta@gmail.com>
2025-11-07 23:41:13 +0700

    first commit

---
+++ build/esp32dev-wifi/lib96b/WebSockets/libb64/cencode.c.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev-wifi/lib96b/WebSockets/libb64/cencode.c.o: \
+ .pio/libdeps/esp32dev-wifi/WebSockets/src/libb64/cencode.c
---
+++ build/esp32dev-wifi/lib96b/WebSockets/libsha1/libsha1.c.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev-wifi/lib96b/WebSockets/libsha1/libsha1.c.o: \
+ .pio/libdeps/esp32dev-wifi/WebSockets/src/libsha1/libsha1.c
